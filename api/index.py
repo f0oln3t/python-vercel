d9af06c741017bc8b41c9fb3cf356776987aca26
FoolNet <164990358+f0oln3t@users.noreply.github.com>
2025-08-13 20:50:58 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,53 +1,56 @@
-from http.server import BaseHTTPRequestHandler
 import json
 
-# Simpan data user dan pesan di memory
-users = {"test": "12345"}  # username: password
-messages = []  # list of dict {username, text}
+users = {"test": "12345"}
+messages = []
 
-class handler(BaseHTTPRequestHandler):
-    def _set_headers(self):
-        self.send_response(200)
-        self.send_header("Content-type", "application/json")
-        self.end_headers()
+def handler(request):
+    path = request.path
+    method = request.method
 
-    def do_GET(self):
-        if self.path == "/messages":
-            self._set_headers()
-            self.wfile.write(json.dumps(messages).encode("utf-8"))
+    if path == "/login" and method == "POST":
+        body = request.json
+        username = body.get("username")
+        password = body.get("password")
+        if username in users and users[username] == password:
+            return {
+                "statusCode": 200,
+                "headers": {"Content-Type": "application/json"},
+                "body": json.dumps({"success": True, "message": "Login berhasil"})
+            }
         else:
-            self._set_headers()
-            self.wfile.write(json.dumps({"success": False, "message": "Not found"}).encode("utf-8"))
+            return {
+                "statusCode": 200,
+                "headers": {"Content-Type": "application/json"},
+                "body": json.dumps({"success": False, "message": "Username atau password salah"})
+            }
 
-    def do_POST(self):
-        content_length = int(self.headers.get('Content-Length', 0))
-        post_data = self.rfile.read(content_length)
-        try:
-            data = json.loads(post_data)
-        except:
-            data = {}
-
-        if self.path == "/login":
-            username = data.get("username")
-            password = data.get("password")
-            if username in users and users[username] == password:
-                self._set_headers()
-                self.wfile.write(json.dumps({"success": True, "message": "Login berhasil"}).encode("utf-8"))
-            else:
-                self._set_headers()
-                self.wfile.write(json.dumps({"success": False, "message": "Username atau password salah"}).encode("utf-8"))
-
-        elif self.path == "/send":
-            username = data.get("username")
-            text = data.get("text")
-            if username and text:
-                messages.append({"username": username, "text": text})
-                self._set_headers()
-                self.wfile.write(json.dumps({"success": True}).encode("utf-8"))
-            else:
-                self._set_headers()
-                self.wfile.write(json.dumps({"success": False, "message": "Data kurang"}).encode("utf-8"))
-
+    elif path == "/send" and method == "POST":
+        body = request.json
+        username = body.get("username")
+        text = body.get("text")
+        if username and text:
+            messages.append({"username": username, "text": text})
+            return {
+                "statusCode": 200,
+                "headers": {"Content-Type": "application/json"},
+                "body": json.dumps({"success": True})
+            }
         else:
-            self._set_headers()
-            self.wfile.write(json.dumps({"success": False, "message": "Not found"}).encode("utf-8"))
+            return {
+                "statusCode": 200,
+                "headers": {"Content-Type": "application/json"},
+                "body": json.dumps({"success": False, "message": "Data kurang"})
+            }
+
+    elif path == "/messages" and method == "GET":
+        return {
+            "statusCode": 200,
+            "headers": {"Content-Type": "application/json"},
+            "body": json.dumps(messages)
+        }
+
+    return {
+        "statusCode": 404,
+        "headers": {"Content-Type": "application/json"},
+        "body": json.dumps({"success": False, "message": "Not found"})
+    }
